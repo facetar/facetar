3a0237ded29871cf9dcd41c4fba31ab96fbe7abd
joh-novelli <joh.novelli90@gmail.com>
2023-12-15 18:18:24 -0300

    teste2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import { Routes, Route, HashRouter } from 'react-router-dom';
-import Main from './pages/Main';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
